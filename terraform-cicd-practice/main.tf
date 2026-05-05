559bceb605e5a9c1d272eeb78ed70e3e7cc05ec3
Eranda Welgama <eeranda@gmail.com>
2026-05-05 16:42:15 +0530

    Format Terraform files

--- terraform-cicd-practice/main.tf
+++ terraform-cicd-practice/main.tf
@@ -10,8 +10,8 @@ resource "aws_s3_bucket" "demo_bucket" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
-    tags = {
-        Name        = "${var.project_name}-vpc"
-        Environment = var.environment
-    }
+  tags = {
+    Name        = "${var.project_name}-vpc"
+    Environment = var.environment
+  }
 }
