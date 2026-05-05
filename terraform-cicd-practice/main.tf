a6a46e8a35b511e8360a494680772f9ce367a05e
Eranda Welgama <eeranda@gmail.com>
2026-05-05 16:35:12 +0530

    Initial Terraform CI/CD project 1st change

--- terraform-cicd-practice/main.tf
+++ terraform-cicd-practice/main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "demo_bucket" {
     Environment = var.environment
   }
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name        = "${var.project_name}-vpc"
+        Environment = var.environment
+    }
+}
