0df32049a5c05cde7de27883ac7b900af9853d27
Eranda Welgama <eeranda@gmail.com>
2026-05-05 21:06:35 +0530

    Add Terraform backend and destroy workflow

---
+++ terraform-backend-bootstrap/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "The AWS region to deploy the backend resources in."
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "backend_bucket_name" {
+  description = "S3 bucket name for storing Terraform state"
+  type        = string
+  default     = "bvs-terraform-state-05052026"
+}
--- terraform-cicd-practice/provider.tf
+++ terraform-cicd-practice/provider.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "s3" {
+    bucket       = "bvs-terraform-state-05052026"
+    key          = "terraform-cicd-practice/dev/terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
