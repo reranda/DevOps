dccc457138f6678b39d3be59bd5d09826f74b813
Eranda Welgama <eeranda@gmail.com>
2026-05-07 12:22:54 +0530

    Terraform formatted files

--- terraform-cicd-practice/provider.tf
+++ terraform-cicd-practice/provider.tf
@@ -1,22 +1,22 @@
 terraform {
-   required_version = ">= 1.6.0"
- 
-   backend "s3" {
-     bucket       = "bvs-terraform-state-07052026"
-     key          = "terraform-cicd-practice/dev/terraform.tfstate"
-     region       = "eu-west-2"
-     encrypt      = true
-     use_lockfile = true
-   }
- 
-   required_providers {
-     aws = {
-       source  = "hashicorp/aws"
-       version = "~> 6.0"
-     }
-   }
+  required_version = ">= 1.6.0"
+
+  backend "s3" {
+    bucket       = "bvs-terraform-state-07052026"
+    key          = "terraform-cicd-practice/dev/terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 }
- 
+
 provider "aws" {
-   region = var.aws_reagion
+  region = var.aws_reagion
 }
--- terraform-cicd-practice/variables.tf
+++ terraform-cicd-practice/variables.tf
@@ -1,23 +1,23 @@
 variable "aws_reagion" {
-   description = "AWS region that deployment is done"
-   type        = string
-   default     = "eu-west-2"
+  description = "AWS region that deployment is done"
+  type        = string
+  default     = "eu-west-2"
 }
- 
+
 variable "bucket_name" {
-   description = "AWS bucket name"
-   type        = string
-   default     = "bvs-terra-remote-07052026"
+  description = "AWS bucket name"
+  type        = string
+  default     = "bvs-terra-remote-07052026"
 }
- 
+
 variable "project_name" {
-   description = "BVS Terraform deployment"
-   type        = string
-   default     = "bvs-terraform-cicd"
+  description = "BVS Terraform deployment"
+  type        = string
+  default     = "bvs-terraform-cicd"
 }
- 
+
 variable "environment" {
-   description = "Deployment environment"
-   type        = string
-   default     = "dev"
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
 }
